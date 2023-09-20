a1fcb61177bb0a5659527943e7fae8e12f5062b6
Chinta Krishna Mourya <118909217+ChintaKrishnaMourya@users.noreply.github.com>
2023-09-20 16:49:38 +0530

    gpt_parser fucntion

--- gptparser.py
+++ gptparser.py
@@ -9,7 +9,7 @@ api_key = st.secrets["api_key"]
 openai.api_key = api_key
 
 def gpt_parser(text):
-  prompt='''You are given a text that is extrcted by using an OCR Tool. The extracted text may sometimes irregular or unclear. So try to understand them interms of bill of lading.
+    prompt='''You are given a text that is extrcted by using an OCR Tool. The extracted text may sometimes irregular or unclear. So try to understand them interms of bill of lading.
   Now, your task is to give right values for the given keys.
   Keys : 
   - "Invoice_Number"
@@ -23,20 +23,26 @@ def gpt_parser(text):
   - "Additional_Notes_or_Terms".
   If no right value found for above keys, give value as "None"
   Stricltly, give final output in JSON format. '''
-  # Record the start time
-  start_time = time.time()
-  completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-16k", 
-    messages = [{"role": "system", "content" : prompt},{"role": "user", "content" : text}])["choices"][0]["message"]["content"]
-  output_json = json.loads(completion) 
+    # Record the start time
+    start_time = time.time()
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k", 
+        messages=[{"role": "system", "content": prompt}, {"role": "user", "content": text}]
+    )["choices"][0]["message"]["content"]
+    
+    # Parse the JSON response
+    try:
+        output_json = json.loads(completion)
+    except json.JSONDecodeError as e:
+        st.error(f"Error parsing JSON: {e}")
+        return None
 
+    df = pd.DataFrame.from_dict(output_json, orient='index') # Load the JSON data into a DataFrame
+    df = df.T # Transpose the DataFrame to have columns as rows
+    # Record the end time
+    end_time = time.time()
 
-  df = pd.read_json(output_json, orient='index') # Load the JSON string into a Pandas DataFrame
-  df = df.T # Transpose the DataFrame to have columns as rows
-  # Record the end time
-  end_time = time.time()
-
-  # Calculate the elapsed time
-  elapsed_time = end_time - start_time
-  st.write(f"Time Taken for Parsing : {elapsed_time}")
-  return df
+    # Calculate the elapsed time
+    elapsed_time = end_time - start_time
+    st.write(f"Time Taken for Parsing : {elapsed_time} seconds")
+    return df
